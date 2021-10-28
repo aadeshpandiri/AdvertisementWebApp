d6fbe06e6936d926130c507aeab2065aba61304d
Aadesh Pandiri <61902161+aadeshpandiri@users.noreply.github.com>
2021-10-29 01:26:23 +0530

    validation to ad form commit

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,4 +1,6 @@
 class Advertisement < ApplicationRecord
     belongs_to:user
     has_many:comments, :dependent => :destroy
+    validates :title,presence:true #{ message: "title is mandatory."  }
+    #validates :title, :desc :phone, :presence => true
 end
